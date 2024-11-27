97d78809f32a5fa1f895fbf31cdbecabe2f7210d
vietdung282002 <87318300+vietdung282002@users.noreply.github.com>
2024-11-28 00:40:56 +0700

    fix bug

--- lib/clock_ui.dart
+++ lib/clock_ui.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:clock_ui/digit_painter.dart';
 import 'package:clock_ui/dot_painter.dart';
-import 'package:clock_ui/segment_painter.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -29,7 +28,7 @@ class _ClockUiState extends State<ClockUi> {
   }
 
   String _getCurrentTime() {
-    return DateFormat('HH:mm:ss').format(DateTime.now());
+    return DateFormat('mm:ss').format(DateTime.now());
   }
 
   @override
@@ -40,9 +39,7 @@ class _ClockUiState extends State<ClockUi> {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
-
+    List<String> digits = currentTime.replaceAll(":", "").split("");
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -50,88 +47,53 @@ class _ClockUiState extends State<ClockUi> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CustomPaint(
-                size: Size(70, 150), // Kích thước vùng vẽ
-                painter: DigitPainter("8"), // Sử dụng CustomPainter
+                size: const Size(70, 150),
+                painter: DigitPainter(digits[0]),
               ),
-              SizedBox(
+              const SizedBox(
                 width: 10,
               ),
               CustomPaint(
-                size: Size(65, 150), // Kích thước vùng vẽ
-                painter: MyCustomPainter(), // Sử dụng CustomPainter
+                size: const Size(70, 150),
+                painter: DigitPainter(digits[1]),
               ),
-              SizedBox(
+              const SizedBox(
                 width: 10,
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   CustomPaint(
-                    size: Size(13, 13), // Kích thước vùng vẽ
-                    painter: DotPainter(), // Sử dụng CustomPainter
+                    size: const Size(13, 13),
+                    painter: DotPainter(),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
                   CustomPaint(
-                    size: Size(13, 13), // Kích thước vùng vẽ
-                    painter: DotPainter(), // Sử dụng CustomPainter
+                    size: const Size(13, 13),
+                    painter: DotPainter(),
                   ),
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 width: 10,
               ),
               CustomPaint(
-                size: Size(65, 150), // Kích thước vùng vẽ
-                painter: MyCustomPainter(), // Sử dụng CustomPainter
+                size: const Size(70, 150),
+                painter: DigitPainter(digits[2]),
               ),
-              SizedBox(
+              const SizedBox(
                 width: 10,
               ),
               CustomPaint(
-                size: Size(65, 150), // Kích thước vùng vẽ
-                painter: MyCustomPainter(), // Sử dụng CustomPainter
+                size: const Size(70, 150),
+                painter: DigitPainter(digits[3]),
               ),
             ],
           ),
         ),
       ),
     );
-  }
-}
-
-class MyCustomPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = Colors.blue
-      ..strokeWidth = 4.0;
-
-    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), paint);
-    paint.color = Colors.red;
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 20, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
-  }
-}
-
-class ClockPainter extends CustomPainter {
-  final String time;
-
-  ClockPainter(this.time);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    return true;
   }
 }
--- lib/digit_painter.dart
+++ lib/digit_painter.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class DigitPainter extends CustomPainter {
@@ -8,16 +6,12 @@ class DigitPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 4.0;
-
     drawDigit(canvas, size, digit);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 
   void drawDigit(Canvas canvas, Size size, String digit) {
@@ -34,121 +28,121 @@ class DigitPainter extends CustomPainter {
       [1, 1, 0, 1, 1, 1, 1], //9
     ];
 
-    final segmentWidth = size.width;
-    final segmentHeight = size.height / 2 - 5;
     final List<int> segments = segmentData[int.parse(digit)];
-    final double segmentThickness = size.height * 8 / 96;
+
+    final double segmentThickness = size.height * 9 / 96;
 
     for (int i = 0; i < segments.length; i++) {
       Color color = Colors.white;
       if (segments[i] == 1) {
         color = Colors.black;
       }
-      // switch (i) {
-      //   case 0:
-      //     drawTopSegment(
-      //         canvas, size, color, segmentWidth, segmentThickness, 0);
-      //   case 1:
-      //     drawLeftSegment(
-      //         canvas, size, color, segmentHeight, segmentThickness, 2, 0, 0);
-      //   case 2:
-      //     drawLeftSegment(canvas, size, color, segmentHeight, segmentThickness,
-      //         5, 0, size.height / 2);
-      //   case 3:
-      //     drawBottomSegment(
-      //         canvas, size, color, segmentWidth, segmentThickness, 0);
-      //   case 4:
-      //     drawRightSegment(
-      //         canvas, size, color, segmentWidth, segmentThickness, 2, 0, 0);
-      //   case 5:
-      //     drawRightSegment(canvas, size, color, segmentWidth, segmentThickness,
-      //         5, 0, size.height / 2);
-      //   // case 6:
-      //   //   drawMidsegment(
-      //   //       canvas, size, color, segmentWidth, segmentThickness, 2);
-      // }
+      switch (i) {
+        case 0:
+          drawTopSegment(canvas, size, color, segmentThickness, 0);
+        case 1:
+          drawLeftSegment(canvas, size, color, segmentThickness, 2, 0);
+        case 2:
+          drawLeftSegment(
+              canvas, size, color, segmentThickness, 2, size.height / 2);
+        case 3:
+          drawBottomSegment(canvas, size, color, segmentThickness, 0);
+        case 4:
+          drawRightSegment(
+              canvas, size, color, segmentThickness, 2, size.height / 2);
+        case 5:
+          drawRightSegment(canvas, size, color, segmentThickness, 2, 0);
+        case 6:
+          drawMidSegment(canvas, size, color, segmentThickness, 2, 4);
+      }
     }
   }
 
-  void drawRect(Canvas canvas, Size size, Color color) {
-    final double segmentThickness = size.height * 8 / 96;
-  }
-
-  void drawTopSegment(Canvas canvas, Size size, Color color, double width,
-      double height, double padding) {
+  void drawTopSegment(
+      Canvas canvas, Size size, Color color, double thickness, double padding) {
+    final x = size.width;
+    final y = size.height;
     Paint paint = Paint()
       ..color = color
       ..style = PaintingStyle.fill;
 
     final path = Path()
       ..moveTo(0, 0)
-      ..lineTo(0 + size.width, 0)
-      ..lineTo(0 + size.width - height, height)
-      ..lineTo(0 + height, height)
+      ..lineTo(x, 0)
+      ..lineTo(x - thickness, thickness)
+      ..lineTo(thickness, thickness)
       ..close();
     canvas.drawPath(path, paint);
   }
 
-  void drawBottomSegment(Canvas canvas, Size size, Color color, double width,
-      double height, double padding) {
+  void drawBottomSegment(
+      Canvas canvas, Size size, Color color, double thickness, double padding) {
+    final x = size.width;
+    final y = size.height;
     Paint paint = Paint()
       ..color = color
       ..style = PaintingStyle.fill;
 
     final path = Path()
-      ..moveTo(0, size.height)
-      ..lineTo(size.width, size.height)
-      ..lineTo(size.width - height, size.height - height)
-      ..lineTo(size.height, size.height - height)
+      ..moveTo(0, y)
+      ..lineTo(x, y)
+      ..lineTo(x - thickness, y - thickness)
+      ..lineTo(thickness, y - thickness)
       ..close();
 
     canvas.drawPath(path, paint);
   }
 
-  void drawLeftSegment(Canvas canvas, Size size, Color color, double width,
-      double height, double padding, double x, double y) {
+  void drawLeftSegment(Canvas canvas, Size size, Color color, double thickness,
+      double padding, double margin) {
+    final x = size.width;
+    final y = size.height;
     Paint paint = Paint()
       ..color = color
       ..style = PaintingStyle.fill;
 
     final path = Path()
-      ..moveTo(0, padding + y)
-      ..lineTo(0, y + size.height / 2 - padding)
-      ..lineTo(height, y + size.height / 2 - padding - height)
-      ..lineTo(height, height + padding + y)
+      ..moveTo(0, margin + padding)
+      ..lineTo(0, margin + y / 2 - padding)
+      ..lineTo(thickness, margin + y / 2 - padding - thickness)
+      ..lineTo(thickness, margin + padding + thickness)
       ..close();
 
     canvas.drawPath(path, paint);
   }
 
-  void drawRightSegment(Canvas canvas, Size size, Color color, double width,
-      double height, double padding, double x, double y) {
+  void drawRightSegment(Canvas canvas, Size size, Color color, double thickness,
+      double padding, double margin) {
+    final x = size.width;
+    final y = size.height;
     Paint paint = Paint()
       ..color = color
       ..style = PaintingStyle.fill;
 
     final path = Path()
-      ..moveTo(size.width, padding + y)
-      ..lineTo(size.width, size.height / 2 - padding + y)
-      ..lineTo(size.width - height, y + size.height / 2 - padding - height)
-      ..lineTo(size.width - height, height + padding + y)
+      ..moveTo(x, margin + padding)
+      ..lineTo(x, margin + y / 2 - padding)
+      ..lineTo(x - thickness, margin + y / 2 - padding - thickness)
+      ..lineTo(x - thickness, margin + padding + thickness)
       ..close();
 
     canvas.drawPath(path, paint);
   }
 
-  void drawMidsegment(Canvas canvas, Size size, Color color, double width,
-      double height, double padding) {
+  void drawMidSegment(Canvas canvas, Size size, Color color, double thickness,
+      double padding, double margin) {
+    final x = size.width;
+    final y = size.height;
     Paint paint = Paint()
       ..color = color
       ..style = PaintingStyle.fill;
     final path = Path()
-      ..moveTo(2, size.height / 2)
-      ..lineTo(height + padding, size.height / 2 - height)
-      ..lineTo(width - height + padding, size.height / 2 - height / 2)
-      ..lineTo(width + padding, size.height / 2)
-      ..lineTo(width - height + padding, size.height / 2 + height / 2)
-      ..lineTo(height + padding, size.height / 2 + height / 2)
+      ..moveTo(0, y / 2)
+      ..lineTo(thickness, y / 2 - thickness + margin)
+      ..lineTo(x - thickness, y / 2 - thickness + margin)
+      ..lineTo(x, y / 2)
+      ..lineTo(x - thickness, y / 2 + thickness - margin)
+      ..lineTo(thickness, y / 2 + thickness - margin)
       ..close();
     canvas.drawPath(path, paint);
   }
